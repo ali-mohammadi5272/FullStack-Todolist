ee8c23ebf9e2779e95d581b96e6404efabade907
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-28 14:32:34 +0330

    refactor: use dotenv for connect to database

--- Back-End/configs/db.js
+++ Back-End/configs/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectToDB = async () => {
   if (mongoose.connections[0].readyState) {
     return false;
   }
   try {
-    await mongoose.connect("mongodb://localhost:27017/todoList-redux");
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
     console.log("Connect to Database successfully :))");
   } catch (error) {
     console.log("Connect to Database faild !!");
