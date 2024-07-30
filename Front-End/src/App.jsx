9800c284c0416131ab736e1cf8b703bc4870de41
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-07-30 13:56:36 +0330

    feat: add ToastContainer

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,10 +1,13 @@
+import { ToastContainer } from "react-toastify";
 import styles from "./App.module.css";
 import Todolist from "./components/Todolist/Todolist";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className={styles.app}>
       <Todolist />
+      <ToastContainer />
     </div>
   );
 }
